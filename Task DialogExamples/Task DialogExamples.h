628b632476304bcce18c43232cbc5313e4784046
wei <w4902e5063i@gmail.com>
2015-09-12 01:41:21 +0800

    麥克風測試1 2 3 第一次沒成功再試一次

---
+++ Task DialogExamples/Task DialogExamples.h
@@ -0,0 +1,32 @@
+
+// Task DialogExamples.h : PROJECT_NAME 應用程式的主要標頭檔
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "對 PCH 包含此檔案前先包含 'stdafx.h'"
+#endif
+
+#include "resource.h"		// 主要符號
+
+
+// CTaskDialogExamplesApp:
+// 請參閱實作此類別的 Task DialogExamples.cpp
+//
+
+class CTaskDialogExamplesApp : public CWinApp
+{
+public:
+	CTaskDialogExamplesApp();
+
+// 覆寫
+public:
+	virtual BOOL InitInstance();
+
+// 程式碼實作
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTaskDialogExamplesApp theApp;
---
+++ Task DialogExamples/Task DialogExamplesDlg.h
@@ -0,0 +1,34 @@
+
+// Task DialogExamplesDlg.h : 標頭檔
+//
+
+#pragma once
+
+
+// CTaskDialogExamplesDlg 對話方塊
+class CTaskDialogExamplesDlg : public CDialogEx
+{
+// 建構
+public:
+	CTaskDialogExamplesDlg(CWnd* pParent = NULL);	// 標準建構函式
+
+// 對話方塊資料
+	enum { IDD = IDD_TASKDIALOGEXAMPLES_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支援
+
+
+// 程式碼實作
+protected:
+	HICON m_hIcon;
+
+	// 產生的訊息對應函式
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+};
